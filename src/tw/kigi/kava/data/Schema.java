d0fdf110a52ec4a11ffd3cb22a585dcc9b41fe35
Kigi Chang <kigi.chang@gmail.com>
2013-07-31 22:05:49 +0800

    2013-07-31

--- src/tw/kigi/kava/data/Schema.java
+++ src/tw/kigi/kava/data/Schema.java
@@ -2,11 +2,27 @@ package tw.kigi.kava.data;
 
 import java.util.HashMap;
 
+import tw.kigi.kava.data.annotation.Table;
+
 public final class Schema {
 
 	protected static HashMap<Class<?>, Schema> schemas = 
 			new HashMap<Class<?>, Schema>();
 	
 	
-
+	private Class<?> clazz;
+	private String tableName;
+	
+	private Schema(Class<?> clazz) {
+		Table table = clazz.getAnnotation(Table.class);
+		tableName = table.name();
+		if (tableName == null || "".equals(tableName = tableName.trim())) {
+			tableName = Convention.toColumnName(tableName);
+		}
+		
+		this.clazz = clazz;
+		
+		
+		
+	}
 }
